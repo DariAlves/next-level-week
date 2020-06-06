693bd9d3c271aeb7ac88b26c556a7d6fbc728458
Dari Alves <darieltondf@gmail.com>
2020-06-05 23:32:00 -0300

    configurando server e criando template engine

---
+++ src/server.js
@@ -0,0 +1,38 @@
+// Criar e iniciar servidor 
+const express = require('express');
+
+const server = express();
+
+//Configurar pasta pública
+server.use(express.static("public"));
+
+// Utilizando Template Engine
+const nunjucks = require('nunjucks');
+nunjucks.configure("src/views", {
+    express: server,
+    noCache: true
+})
+
+
+
+// Configurar caminhos da minha aplicação
+// Página incial 
+// Req: requisição
+// Res: resposta
+
+server.get("/", (req, res) => {
+    return res.render("index.html");
+})
+
+server.get("/create-point", (req, res) => {
+    return  res.render("create-point.html");
+})
+
+server.get("/search", (req, res) => {
+    return  res.render("search-results.html");
+})
+
+
+// ligar o servidor
+server.listen(3000);
+
